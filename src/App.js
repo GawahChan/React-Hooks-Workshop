d72d262fa6934d379091b009a331cf6c08ef88cf
Gawah Chan <gawahchan@gmail.com>
2019-07-05 10:50:48 +0100

    Added dynamic name input box

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import './App.css';
 
 function App() {
   const[count, setCount] = useState(0);
+  const[name, setName] = useState("")
 
   return (
     <div className="App">
       <h1>Hello World</h1>
 
-      <h3>This is my counter: {count}</h3>
+      <h3>Hi, my name is {name} and this is my counter: {count}</h3>
 
-      <button onClick={() => setCount(count + 1) }>Add by 1</button>
-      <button onClick={() => setCount(count - 1) }>Takeaway by 1</button>
+      <h6>
+        Name: <input 
+          className="nameInput"
+          type="text" 
+          onChange={(e) => setName(e.target.value)}
+        />
+      </h6>
+      <button onClick={() => setCount(count + 1)}>Add by 1</button>
+      <button onClick={() => setCount(count - 1)}>Takeaway by 1</button>
     </div>
   );
 }
